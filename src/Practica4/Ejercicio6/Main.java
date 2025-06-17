cd5d774706a2dacf852171803f0822643e8d5ad4
Emy <emily.galeas@epn.edu.ec>
2025-06-16 19:26:01 -0500

    MAIN EJERCICIO 6, PRACTICA 4

--- src/Practica4/Ejercicio6/Main.java
+++ src/Practica4/Ejercicio6/Main.java
@@ -2,5 +2,8 @@ package Practica4.Ejercicio6;
 
 public class Main {
     public static void main(String[] args){
+        Usuario usuario = new Usuario();
+        usuario.setPassword("secreto123");
+        usuario.setPassword("123");
     }
 }
